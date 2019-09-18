d4516f2d2572239d6a0f7f530a84f65074c773f0
KevinCastro <castrok.c11@hotmail.com>
2019-09-18 22:20:07 +0000

    2nd program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
