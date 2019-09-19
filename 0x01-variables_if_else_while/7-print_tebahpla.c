e17c3fe0fb3918f4e42653a557468c72df288d27
KevinCastro <castrok.c11@hotmail.com>
2019-09-19 22:43:48 +0000

    Z-A

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ * Return: 0
+ * Description:  n is positive, negative or zero
+ */
+
+int main(void)
+{
+int n = 97;
+
+for (n = 122; n > 96; n--)
+{
+putchar(n);
+}
+putchar(10);
+
+return (0);
+}
