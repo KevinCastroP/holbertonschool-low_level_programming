7b29f3d4c22a213f1b0d41e8fe51dba2090e2e10
KevinCastro <castrok.c11@hotmail.com>
2019-09-19 22:20:22 +0000

    alp

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ * Return: 0
+ * Description:  n is positive, negative or zero
+ */
+
+int main(void)
+{
+int n = 97;
+
+for (n = 97; n < 123; n++)
+{
+
+putchar(n);
+}
+for (n = 65; n < 91; n++)
+{
+
+putchar(n);
+}
+
+putchar(10);
+
+return (0);
+}
