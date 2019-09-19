95921fe508cd8ca6ad1122a53cd2c351faef6781
KevinCastro <castrok.c11@hotmail.com>
2019-09-19 20:35:18 +0000

    if conditional

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,32 @@
+
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ * Return: 0
+ * Description:  n is positive, negative or zero
+ */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
-  int n;
+int n;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  /* your code goes there */
-  return (0);
- }
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
+}
