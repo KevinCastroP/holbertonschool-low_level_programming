dbfd474c6d491fd1d12da0aa6a4a3d9ae4d9e7bb
KevinCastro <castrok.c11@hotmail.com>
2019-09-24 14:11:38 +0000

    1st time using my library

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int main(void)
+
+{
+_putchar ('H');
+_putchar ('o');
+_putchar ('l');
+_putchar ('b');
+_putchar ('e');
+_putchar ('r');
+_putchar ('t');
+_putchar ('o');
+_putchar ('n');
+_putchar ('\n');
+
+return (0);
+}
