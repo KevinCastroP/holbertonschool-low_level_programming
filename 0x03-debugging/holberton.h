0a7661f30625fbd6346e89ce40cba6d0612d6551
KevinCastro <castrok.c11@hotmail.com>
2019-09-25 18:37:23 +0000

    leap year

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -8,3 +8,9 @@ void positive_or_negative(int i);
 #endif /* HOLBERTON_H */
 
 int largest_number(int a, int b, int c);
+
+/*convert a day in an int*/
+int convert_day(int month, int day);
+
+/*print rest days of a year*/
+void print_remaining_days(int month, int day, int year);
