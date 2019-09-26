d6f178695f18bed9a9e5b7b6815a23355af1066c
KevinCastro <castrok.c11@hotmail.com>
2019-09-26 18:39:59 +0000

    check a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _isdigit - Function that checks for uppercase character.
+ * @k: Check value return.
+ * Return: 1 if character == digit and 0 else.
+ */
+
+int _isdigit(int k)
+{
+if (k >= 0 && k <= 9)
+{
+return (1);
+}
+return (0);
+}
