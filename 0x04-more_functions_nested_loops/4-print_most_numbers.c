cde66e17b36cbfc6441186865e919889a96202cd
KevinCastro <castrok.c11@hotmail.com>
2019-09-26 21:06:36 +0000

    except 2 #

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,11 +9,14 @@
 void print_most_numbers(void)
 
 {
-int k = '0';
+int k;
 
-if (k < 2 && k > 4 && k <= 9)
+for(k = 0; k <= 9; k++) 
 {
-_putchar('k');
+  if (!(k == 2 || k == 4))
+{
+_putchar(k + '0');
+}
 }
 _putchar('\n');
 
