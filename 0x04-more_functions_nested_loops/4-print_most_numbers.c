322c7f4cbbb0507c26b937a4dee1c1256a2e1046
KevinCastro <castrok.c11@hotmail.com>
2019-09-26 21:13:39 +0000

    except 2 #

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,18 +2,16 @@
 
 /**
  * print_most_numbers - check the code for Holberton School students.
- * @k: Desc.
  * Return: null.
  */
 
 void print_most_numbers(void)
-
 {
 int k;
 
-for(k = 0; k <= 9; k++) 
+for (k = 0; k <= 9; k++)
 {
-  if (!(k == 2 || k == 4))
+if (!(k == 2 || k == 4))
 {
 _putchar(k + '0');
 }
