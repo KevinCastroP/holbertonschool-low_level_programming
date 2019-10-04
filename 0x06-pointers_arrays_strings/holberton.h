db21a17696401e59b2ff60c6ef9e505162ca2469
KevCasHSC <castrok.c11@hotmail.com>
2019-10-04 03:15:37 +0000

    capitalizes

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,2 +1,18 @@
 /*appends strings*/
 char *_strcat(char *dest, char *src);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n);
+
+/*compare 2 strings*/
+int _strcmp(char *s1, char *s2);
+
+/*reverse the content*/
+void reverse_array(int *a, int n);
+
+/*changes letters*/
+char *string_toupper(char *);
+
+/*capitalize*/
+char *cap_string(char *);
