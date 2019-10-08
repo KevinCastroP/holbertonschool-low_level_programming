50eef3601a8094ed9f6058be8a371088bbc8fd20
KevCasHSC <castrok.c11@hotmail.com>
2019-10-08 04:23:46 +0000

    locate substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,49 +1,30 @@
 #include "holberton.h"
-#include <stdio.h>
-#include <strings.h>
-
 /**
- * _strstr - function that locates a substring.
- * @haystack :   input string
- * @needle:  character to find firts occurrence
- * Return:Returns a pointer to the byte in s that matches one of the bytes in
- *accept, or NULL if no such byte is found
- * On error, -1 is returned, and errno is set appropriately.
- */
+ * _strstr - this function found the bytes of a string in other string
+ * @haystack: this is the array contains the all string
+ * @needle: this containes the bytes to found
+ (* a blank line
+ * Description: this found the bytes of a string in other)?
+(* section header: the header of this function is holberton.h)*
+* Return: return a char;
+*/
 
 char *_strstr(char *haystack, char *needle)
 {
-unsigned int i = 0;
-unsigned int j = 0;
-int sw = 0;
-int swant = 0;
 
-for (i = 0 ; needle[i] != '\0'; i++)
+while (*haystack != '\0')
 {
-sw = 0;
-for (j = 0; haystack[j] != '\0'; j++)
-{
-if (needle[i] == haystack[j])
-{
-return (needle);
-sw = 1;
-swant = 1;
-}
+char *i = haystack;
+char *j = needle;
 
-else
+while (*j == *haystack && *j != '\0' && *haystack != '\0')
 {
-swant = 0;
+haystack++;
+j++;
 }
+if (*j == '\0')
+return (i);
+haystack = i + 1;
 }
-if (sw == 1 && swant == 0)
-{
-break;
-}
-else
-{
-continue;
-}
-}
-
-return (NULL);
+return (0);
 }
