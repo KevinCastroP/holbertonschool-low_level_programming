57816b5dd14c343fb957be554aaad32d1a648201
KevCasHSC <castrok.c11@hotmail.com>
2019-10-13 03:03:02 +0000

    locate a char in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stddef.h>
+
+/**
+ *_strchr - concatnate
+ *@s: dest pointer
+ *@c: src pointer
+ *Return: something
+ */
+
+char *_strchr(char *s, char c)
+{
+
+int k;
+
+for (k = 0; s[k] != '\0'; k++)
+{
+if (s[k] == c)
+{
+return (s + k);
+}
+}
+
+return (NULL);
+
+}
