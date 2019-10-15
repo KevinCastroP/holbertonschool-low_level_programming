fffdcb596e64a3a9902e2f402e8b9d06071ff322
KevinCastro <castrok.c11@hotmail.com>
2019-10-15 22:39:14 +0000

    concatenate

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,5 +1,8 @@
 /*asjas*/
 char *create_array(unsigned int size, char c);
 
-/*newly allocated*/
+/*copy*/
 char *_strdup(char *str);
+
+/*concatenates 2 strings*/
+char *str_concat(char *s1, char *s2);
