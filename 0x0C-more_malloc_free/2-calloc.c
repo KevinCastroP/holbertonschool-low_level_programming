656e26d4ab0e31b1a15b63b0297346b2eba015a2
KevCasHSC <castrok.c11@hotmail.com>
2019-10-17 23:07:44 +0000

    error

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,32 +1,44 @@
 #include "holberton.h"
-
 /**
- * _calloc - allocaes memory for an array, using malloc, and set all
- * entries with 0.
- * @nmemb: is the number of elements of the array
- * @size: is the size of bytes of each element
- * Return: a pointer to the memory space.
- */
-
-void *_calloc(unsigned int nmemb, unsigned int size)
+ * _memset - this function set memory
+ * @s: s is a buffer that it contains in array
+ * @b: the new value to set in the buffer
+ * @n: the number of bytes to be altered.
+(* a blank line
+* Description: this function set memory)?
+(* section header: the header of this function is holberton.h)*
+* Return: return char
+*/
+char *_memset(char *s, char b, unsigned int n)
 {
+int i;
 
-unsigned int c;
-char *arr;
-
-if ((nmemb == 0) || (size == 0))
-return (NULL);
-
-arr = malloc(nmemb * size);
-
-if (arr == NULL)
+for (i = 0; n > 0; i++, n--)
 {
-return (NULL);
+s[i] = b;
 }
 
-for (c = 0; c < nmemb * size; c++)
-arr[c] = '\0';
+return (s);
+}
+/**
+ * _calloc - allocate memory and set in 0.
+(* a blank line
+*@nmemb:the size of the data type
+*@size: number of btyes to be alocated
+* Description: copy a string)?
+(* section header: the header of this function is holberton.h)*
+* Return: no return only allocate memory.
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *p;
 
-return (arr);
+if (nmemb == 0 || size == 0)
+return (NULL);
+p = malloc(nmemb * size);
+if (p == NULL)
+return (NULL);
+_memset(p, 0, (nmemb * size));
 
+return (p);
 }
