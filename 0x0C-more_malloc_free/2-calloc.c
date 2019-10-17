0ecd69c76692af58f0886f33a72d98877b90c495
KevCasHSC <castrok.c11@hotmail.com>
2019-10-17 23:30:57 +0000

    kk

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,44 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "holberton.h"
-/**
- * _memset - this function set memory
- * @s: s is a buffer that it contains in array
- * @b: the new value to set in the buffer
- * @n: the number of bytes to be altered.
-(* a blank line
-* Description: this function set memory)?
-(* section header: the header of this function is holberton.h)*
-* Return: return char
-*/
-char *_memset(char *s, char b, unsigned int n)
-{
-int i;
 
-for (i = 0; n > 0; i++, n--)
-{
-s[i] = b;
-}
-
-return (s);
-}
 /**
- * _calloc - allocate memory and set in 0.
-(* a blank line
-*@nmemb:the size of the data type
-*@size: number of btyes to be alocated
-* Description: copy a string)?
-(* section header: the header of this function is holberton.h)*
-* Return: no return only allocate memory.
-*/
+ * _calloc - Function to allocate memory for an array initialized in 0
+ * @nmemb: First parameter passed to function to process
+ * @size: Second parameter passed to function to process
+ * Return: A pointer to the allocated memory for the array of @nmemb elements
+ * of @size bytes, or NULL if size is 0
+ */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-void *p;
+
+void *matrix;
+char *ptr;
+unsigned int i;
 
 if (nmemb == 0 || size == 0)
 return (NULL);
-p = malloc(nmemb * size);
-if (p == NULL)
+matrix = malloc(size * nmemb);
+if (matrix == NULL)
 return (NULL);
-_memset(p, 0, (nmemb * size));
+ptr = matrix;
+for (i = 0; i < (nmemb * size); i++)
+{
+ptr[i] = 0;
+}
+return (ptr);
 
-return (p);
 }
