d0e933e28a7f9ec14e1be364c665048e0412f9a0
KevCasHSC <castrok.c11@hotmail.com>
2019-10-22 04:43:44 +0000

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * free_dog - frees dogs
+ * @d: the dog struct
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+if (d->name != NULL)
+free(d->name);
+if (d->owner != NULL)
+free(d->owner);
+free(d);
+}
+}
