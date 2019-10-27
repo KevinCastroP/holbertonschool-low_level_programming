f5ff2554ae04a85828e88423bd8b86227b44138f
KevCasHSC <castrok.c11@hotmail.com>
2019-10-27 03:00:19 +0000

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,7 +14,7 @@ void print_all(const char * const format, ...)
 	char *m, *b = (char *)format;
 
 	va_start(kclist, format);
-	while (*b != '\0' && format != NULL)
+	while (*b != '\0' && b != NULL)
 	{
 		switch (*(b))
 		{
