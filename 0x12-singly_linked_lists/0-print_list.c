c074c512ce0e8acc3f18ff48e9327d9b50870848
KevinCastro <castrok.c11@hotmail.com>
2019-10-31 21:40:49 +0000

    last upload

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * print_list - function printer all nodos in a list
+ * @h: pointer of list
+ * @str: contain string to print
+ * Return: number of nodos
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL){
+		count++;
+		if (h->str == NULL){
+			printf("[0] (nil)\n");
+		}
+		else{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+	}
+	return (count);
+}
