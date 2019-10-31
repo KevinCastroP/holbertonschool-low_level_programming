239a742a441337ebe6ad48918d9f4371c42d3ab8
KevinCastro <castrok.c11@hotmail.com>
2019-10-31 23:46:26 +0000

    last upload

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -26,4 +26,6 @@ size_t print_list(const list_t *h);
 int _putchar(char c);
 /*function to return the # of elements in a linked list*/
 size_t list_len(const list_t *h);
+/*add a new node at the beginning of a list*/
+list_t *add_node(list_t **head, const char *str);
 #endif
