59ab4782fbce36018371577a2203067913fd2c2f
KevinCastro <castrok.c11@hotmail.com>
2019-11-01 14:42:13 +0000

    last upload

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -30,4 +30,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 /*add a new node at the end of a list*/
 list_t *add_node_end(list_t **head, const char *str);
+/*function to free list*/
+void free_list(list_t *head);
 #endif
