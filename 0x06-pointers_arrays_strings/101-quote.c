671d589e5e923d126c7f98246ba48b42aed35e16
KevinCastro <castrok.c11@hotmail.com>
2019-11-01 18:14:39 +0000

    last upload

---
+++ 0x06-pointers_arrays_strings/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int l;
+
+	l = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      l);
+	return (1);
+}
