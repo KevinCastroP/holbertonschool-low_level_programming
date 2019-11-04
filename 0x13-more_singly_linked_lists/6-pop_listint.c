a4b3df41e1ac3a89b9d3adc7ce09e1db15f2994c
KevCasHSC <castrok.c11@hotmail.com>
2019-11-04 20:20:51 +0000

    last upload

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * pop_listsint - function to delete the head node
+ * @head: pointer to pointer to NULL
+ *
+ * Return: a value
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *copy;
+	int c;
+
+	if (*head != NULL)
+	{
+		copy = (*head)->next;
+		c = (*head)->n;
+		free(*head);
+		*head = copy;
+		return (c);
+	}
+	else
+		return (0);
+}
