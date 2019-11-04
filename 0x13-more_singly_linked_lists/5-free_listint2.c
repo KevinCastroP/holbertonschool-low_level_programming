b76a72f197ec9b414445e811c50337e342136da8
KevCasHSC <castrok.c11@hotmail.com>
2019-11-04 19:56:34 +0000

    last upload

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_listint2 - function to free a list
+ * @head: pointer to pointer to NULL
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *copy;
+
+	if (*head != NULL)
+	{
+		while ((*head)->next != NULL)
+		{
+			copy = (*head)->next;
+			free(*head);
+			*head = copy;
+		}
+		free(*head);
+		*head = NULL;
+	}
+}
