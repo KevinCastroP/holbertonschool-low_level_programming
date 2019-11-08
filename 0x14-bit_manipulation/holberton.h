1a60c59d5296c3e3ba6c545357843c0e2a513c42
KevinCastro <castrok.c11@hotmail.com>
2019-11-08 19:57:53 +0000

    last upload

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -12,4 +12,10 @@ unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 /*function to return the value of a bit at a given index*/
 int get_bit(unsigned long int n, unsigned int index);
+/*function to sets the value of a bit to 1 at a given index*/
+int set_bit(unsigned long int *n, unsigned int index);
+/*function to set the value of a bit to 0 at a given index*/
+int clear_bit(unsigned long int *n, unsigned int index);
+/*function to return the number of bits you need to flip get one # to another*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 #endif
