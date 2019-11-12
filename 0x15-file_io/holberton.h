73e7b6c358281fb78033009fe847b7b15c3b838d
KevinCastro <castrok.c11@hotmail.com>
2019-11-12 19:26:58 +0000

    last upload

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,10 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/*function to print*/
+int _putchar(char c);
+#endif
