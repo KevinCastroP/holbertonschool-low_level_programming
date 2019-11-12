429e54f529cb7fb8fea734fbbc0d82303748fa45
KevinCastro <castrok.c11@hotmail.com>
2019-11-12 21:22:13 +0000

    last upload

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -9,4 +9,6 @@
 int _putchar(char c);
 /*function to read a text file and print it to POSIX*/
 ssize_t read_textfile(const char *filename, size_t letters);
+/*function to create a file*/
+int create_file(const char *filename, char *text_content);
 #endif
