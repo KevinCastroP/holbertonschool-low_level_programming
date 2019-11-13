ea977cf9155ab82dd3a5fdf84ff7f461a482f0ef
KevinCastro <castrok.c11@hotmail.com>
2019-11-13 19:10:31 +0000

    last upload

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -13,4 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 /*function to appends text at the end of a file*/
 int append_text_to_file(const char *filename, char *text_content);
+void _cant_read(char *file_name);
+void _cant_write(char *file_name);
+void _close_FD(int file_from, int file_to);
 #endif
