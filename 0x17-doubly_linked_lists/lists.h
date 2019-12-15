f36ff1cc4c27d73036e4808636650498d5fb289d
KevCasHSC <castrok.c11@hotmail.com>
2019-12-15 05:14:57 +0000

    last upload

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef _LISTS_H
+#define _LISTS_H
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+/*function to print all the elements of a dlist*/
+size_t print_dlistint(const dlistint_t *h);
+#endif
