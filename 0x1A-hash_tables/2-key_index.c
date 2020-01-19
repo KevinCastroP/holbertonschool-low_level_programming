25874ec94ac7cbf934ee2b231cd86d08297b5ae3
KevCasHSC <castrok.c11@hotmail.com>
2020-01-19 17:50:14 +0000

    last upload

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+/**
+ * key_index - function to give you the index of a key
+ * @key: pointer
+ * @size: size of the array of the hash table
+ * Return: the index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned int index_key = hash_djb2(key) % size;
+	return(index_key);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -35,4 +35,6 @@ typedef struct hash_table_s
 hash_table_t *hash_table_create(unsigned long int size);
 /*implementing the djb2 algorithm*/
 unsigned long int hash_djb2(const unsigned char *str);
+/*function to gives the index of a key*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 #endif /*HASH_TABLES_H*/
